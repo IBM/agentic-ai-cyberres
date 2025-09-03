e93ab8d54165a8adf6ca781df2f119ef79934bab
NavdeepSinghBagga <navdeepsinghbagga1999@gmail.com>
2025-09-03 14:54:52 +0530

    added memory persistence using SQLChatMessageHistory

--- python/src/main.py
+++ python/src/main.py
@@ -4,7 +4,7 @@
 import os
 import asyncio
 from reader import create_console_reader
-from llm import get_chat_llm, Providers, SafetyViolationError
+from llm import ChatLLM, get_chat_llm, Providers, SafetyViolationError
 from agent import BeeAgent, TokenMemory
 from dataValidatorTools import (
     FindRunningProcessesTool,
@@ -12,6 +12,10 @@ from dataValidatorTools import (
     SendEmailTool,
     FindWhatsRunningByPortsTool
 )
+from beeai_framework.memory import TokenMemory
+from beeai_framework.backend import AssistantMessage
+from beeai_framework.backend import UserMessage
+from langchain_community.chat_message_histories import SQLChatMessageHistory
 
 class SessionGuardrails:
     @staticmethod
@@ -24,9 +28,27 @@ class SessionGuardrails:
             print(f"Warning: Missing environment variables: {', '.join(missing_vars)}")
             print("Some features may not work properly.")
 
+def get_conv_history():
+    message_history = SQLChatMessageHistory(
+        session_id='conv_session', connection_string='sqlite:///my_chat_history.db'
+    )
+    return message_history
+
+async def load_memory(chat_model: ChatLLM, conv_history: SQLChatMessageHistory ):
+    memory = TokenMemory(chat_model)
+    messages = conv_history.get_messages()
+    for message in messages:
+        if message.type == "human":
+            await memory.add(UserMessage(content=message.content))
+        if message.type == "ai":
+            await memory.add(AssistantMessage(content=message.content))
+    return memory
+
 async def main():
     # Validate environment
     SessionGuardrails.validate_environment()
+    
+    conv_history: SQLChatMessageHistory = get_conv_history()
     
     # Get LLM instance
     try:
@@ -38,7 +60,7 @@ async def main():
     # Create agent with tools
     agent = BeeAgent(
         llm=llm,
-        memory=TokenMemory(llm=llm),
+        memory = await load_memory(llm, conv_history),
         tools=[
             FindRunningProcessesTool,
             MongoDBDataValidatorTool,
@@ -66,6 +88,11 @@ async def main():
             
             reader.write("Agent 🤖", response["result"]["text"])
             
+            # TODO: can add a check if the answer is a in-between step then skip it.
+            # check to only add final answer to the memory
+            conv_history.add_user_message(prompt_data["prompt"])
+            conv_history.add_ai_message(response["result"]["text"])
+
         except SafetyViolationError as e:
             reader.write("Security Error", f"Blocked: {str(e)}")
         except Exception as error:
